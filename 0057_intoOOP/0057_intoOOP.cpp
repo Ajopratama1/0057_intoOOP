fd65d3a752b63eda8d0c45e5529d68c1949ed12d
Ajopratama1 <a.ajopratama01@gmail.com>
2024-04-22 16:00:08 +0700

    Inisialisasi objek Mahasiswa dan Matakuliah dalam main()

--- 0057_intoOOP/0057_intoOOP.cpp
+++ 0057_intoOOP/0057_intoOOP.cpp
@@ -35,3 +35,13 @@ class Matakuliah {
 		 cout << "SKS: " << sks << endl;
 	 }
 };
+
+int main() {
+	Mahasiswa mhs;
+	Matakuliah mk;
+
+	mhs.nama = "Busi";
+	mhs.umur = 20;
+	mhs.jurusan = "Hukum";
+	
+}
